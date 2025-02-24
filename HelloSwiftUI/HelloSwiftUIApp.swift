f40954e767917a3de2d3e2712197f051b2e6804e
TvT669 <3172736837@qq.com>
2025-02-24 16:53:12 +0800

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloSwiftUITests/HelloSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUITests.swift
+//  HelloSwiftUITests
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/2/24.
+//
+
+import Testing
+@testable import HelloSwiftUI
+
+struct HelloSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
